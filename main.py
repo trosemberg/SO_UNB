81171d81afe69c4534920e68d73861ca77dd90b3
trosemberg <tomasroro@hotmail.com>
2018-11-21 23:57:27 -0200

    indo mas li errado uma parte das especs, tenho q arrumar muita coisa

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import sys
+import processos as gproc
+import memoria as gmem
+import es as ges
+import arquivos as garq
+"""
+    Os imports estão sendo feitos como g+primeira letra do import pois é gerenciador de...
+    es = entrada e saida
+"""
+
 def start():
     fileProc = "processes.txt"
     fileFiles = "files.txt"
@@ -7,5 +16,6 @@ def start():
         fileFiles = sys.argv[2]
     process = open(fileProc,"r")
     files = open(fileFiles, "r")
+    
 if __name__ == '__main__':
     start()
---
+++ processos.py
@@ -0,0 +1,10 @@
+class Processos:
+    def __init__(self):
+        self.t_init = 0
+        self.prio = 0
+        self.t_proc = 0
+        self.mem_bloc = 0
+        self.impressora = 0
+        self.scanner = 0
+        self.modem = 0
+        self.disco = 0
