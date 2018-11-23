bd19cd97b55dab5214f6d0ea81ae7c5c9790f447
trosemberg <tomasroro@hotmail.com>
2018-11-23 21:18:47 -0200

    trabalhando com a memoria agora

--- memoria.py
+++ memoria.py
@@ -4,3 +4,10 @@ USER_SIZE = 960
 class G_Memoria:
     def __init__(self):
         self.memoria = [0 for _ in range(0, REAL_SIZE+USER_SIZE)]
+    def aloca_processo(self,processo):
+    
+    # def retira_processo(self,processo):
+    
+    def limpa_memoria_usuario(self,processo):
+        self.memoria[REAL_SIZE:] =  USER_SIZE * [0]
+    
